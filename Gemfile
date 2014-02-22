d3a7c6b8b359387d6ce39985616221c95bec05ac
Damian Galarza <galarza.d@gmail.com>
2014-02-22 11:50:03 -0500

    Remove sensitive data from repo

--- Gemfile
+++ Gemfile
@@ -1,11 +1,8 @@
 source 'https://rubygems.org'
 
-gem "middleman", "~>3.1.5"
-gem 'middleman-s3_sync'
+gem 'dotenv'
+gem 'middleman', '~>3.1.5'
 gem 'middleman-cloudfront'
-
-gem "middleman-livereload", "~> 3.1.0"
-
-gem 'capistrano'
-
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+gem 'middleman-livereload', '~> 3.1.0'
+gem 'middleman-s3_sync'
+gem 'wdm', '~> 0.1.0', :platforms => [:mswin, :mingw]
---
+++ config.rb
@@ -0,0 +1,31 @@
+require 'dotenv'
+Dotenv.load
+
+set :haml, { :ugly => true, :format => :html5 }
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+activate :directory_indexes
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :gzip
+end
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = ENV['AWS_S3_BUCKET']
+  s3_sync.aws_access_key_id          = ENV['AWS_ACCESS_KEY']
+  s3_sync.aws_secret_access_key      = ENV['AWS_SECRET_ACCESS_KEY']
+  s3_sync.delete                     = false
+  s3_sync.after_build                = false
+  s3_sync.prefer_gzip                = true
+  s3_sync.reduced_redundancy_storage = false
+end
+
+caching_policy 'text/css',               max_age: 315360000, must_revalidate: false
+caching_policy 'application/javascript', max_age: 315360000, must_revalidate: false
+caching_policy 'image/png',              max_age: 315360000, must_revalidate: false
